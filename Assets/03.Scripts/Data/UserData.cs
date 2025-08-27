60011bb3c6131c63b0600f8c77090b035232c811
jo-ji-hyun <newterr@naver.com>
2025-08-27 17:20:57 +0900

    [Feat] 데이터 저장 완료

---
+++ Assets/03.Scripts/Data/UserData.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class UserData 
+{
+    public int stage;    // === 진행 상황 ===
+    public int money;    // === 소지 금 ===
+}
---
+++ Assets/03.Scripts/Manager/DataManager.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.SocialPlatforms.Impl;
+
+public class DataManager : Singleton<DataManager>
+{
+    public UserData userData;
+
+    protected override bool IsDestroy => false;
+
+    private string _filePath;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        // === 파일 경로를 찾기 ===
+        _filePath = Path.Combine(Application.persistentDataPath, "userData.json");
+
+        LoadData();
+    }
+
+    public void LoadData()
+    {
+        // === 파일 존재시 ===
+        if (File.Exists(_filePath))
+        {
+            var loadData = File.ReadAllText(_filePath);
+
+            userData = JsonUtility.FromJson<UserData>(loadData);
+        }
+        else // === 없으면 새로만듬 ===
+        {
+            userData = new UserData 
+            {
+                stage = 1, money = 0
+            };
+
+            string json = JsonUtility.ToJson(userData);
+
+            File.WriteAllText(_filePath, json);
+
+            SaveData(userData);
+        }
+    }
+
+    public void SaveData(UserData data)
+    {
+        var saveData = JsonUtility.ToJson(data);
+
+        File.WriteAllText(_filePath, saveData);
+    }
+}
