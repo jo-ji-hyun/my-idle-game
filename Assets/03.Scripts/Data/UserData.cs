d51b14b4338c1107fab4deff6ea1405f3e5f2fd1
jo-ji-hyun <newterr@naver.com>
2025-08-28 11:28:11 +0900

    [Feat] 플레이어 스펙 정리
    공 방 체 크리티컬 추가

--- Assets/03.Scripts/Data/UserData.cs
+++ Assets/03.Scripts/Data/UserData.cs
@@ -9,4 +9,10 @@ public class UserData
     public int stage;    // === 진행 상황 ===
     public int bossHp;   // === 보스 hp ===
     public int money;    // === 소지 금 ===
+
+    // === 플레이어 스펙 ===
+    public int Atk;
+    public int Def;
+    public int HP;
+    public int Cri;
 }
