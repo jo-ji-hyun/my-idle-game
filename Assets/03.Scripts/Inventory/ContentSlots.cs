ac4d0cd8ea1959a0d43f938f0433db814a3c30a9
jo-ji-hyun <newterr@naver.com>
2025-08-29 12:26:04 +0900

    [Feat]인벤토리 구성 완료

--- Assets/03.Scripts/Inventory/ContentSlots.cs
+++ Assets/03.Scripts/Inventory/ContentSlots.cs
@@ -4,5 +4,45 @@ using UnityEngine;
 
 public class ContentSlots : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject slot;
 
+    private int _slotCount = 20;
+
+    public List<GameObject> _slotList;
+
+    private void Start()
+    {
+        if (UIManager.Instance != null)
+        {
+            SetItem();
+        }
+    }
+
+    void SetItem()
+    {
+        _slotList = new List<GameObject>();
+
+        for (int i = 0; i < _slotCount; i++)
+        {
+            GameObject slotPrefabs = Instantiate(slot, transform);
+            _slotList.Add(slotPrefabs);
+        }
+
+        RefreshUI();
+    }
+
+    void RefreshUI()
+    {
+        for (int i = 0; i < _slotCount; i++)
+        {
+            // === id¸¦ ºÎ¿© ===
+            Slot slot = _slotList[i].GetComponent<Slot>();
+
+            //if (i < GameManager.Instance.allitems.Count)
+            //{
+            //    slot.id = i;
+            //}
+        }
+    }
 }
--- Assets/03.Scripts/Inventory/Slot.cs
+++ Assets/03.Scripts/Inventory/Slot.cs
@@ -1,18 +1,38 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Slot : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public int id;                             // === 아이템 식별 번호 ===
+
+    [Header("Slot")]
+    public Image icon;                         // === 아이콘 표시 ===
+    public TextMeshProUGUI enhancedStatus;     // === 장착중 표시 ===
+
+    [Header("DescriptionText")]
+    public Image descriptionText;              // === 설명창 ===
+
+    private void Start()
     {
-        
+        // icon.sprite = GameManager.Instance.allitems[id].icon;
+
+        UpdateStatus();
     }
 
-    // Update is called once per frame
-    void Update()
+    // === 강화된 데이터 받아오기 ===
+    private void UpdateStatus()
     {
-        
+       // enhancedStatus.text = () ? $"{E}" : "0";
+    }
+
+    // === 버튼 클릭시 호출 ===
+    public void OnClick()
+    {
+        GameObject descriptionText = GameObject.Find("DescriptionWindow");
+
+        descriptionText.SetActive(true); // === 나중에 텍스트를 넘겨주는 쪽으로 수정 ===
     }
 }
