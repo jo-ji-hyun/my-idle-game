d139f4d1b46c92919f48641cc6cb33689281f8d8
jo-ji-hyun <newterr@naver.com>
2025-11-21 20:38:52 +0900

    [Feat] 서버에서 패치 리소스를 받기 위해 타이틀씬 생성

---
+++ Assets/03.Scripts/Ui/TitleUi.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitleUi : MonoBehaviour
+{
+    public Button ExitBtn;
+    public Slider LoadingBar;
+
+    [Header("Loading")]
+    public TextMeshProUGUI NewAmountTxt;
+    [SerializeField] private long Amount;
+    public Button DownLoadBtn;
+    public Button CancelBtn;
+    public GameObject LoadingPannel;
+
+    private void Start()
+    {        
+        //ExitBtn.onClick.AddListener();
+
+        DownLoadBtn.onClick.AddListener(OnStartDownLoadButton);
+    }
+
+    private void OnStartDownLoadButton()
+    {
+        LoadingPannel.SetActive(true);
+    }
+}
---
+++ Assets/03.Scripts/Utility/DownLoadFiles.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DownLoadFiles : MonoBehaviour
+{
+    private long _amount;
+    public TextMeshProUGUI AmountTxt;
+    public TextMeshProUGUI PercentageTxt;
+    public Slider ProgressBar;
+
+}
