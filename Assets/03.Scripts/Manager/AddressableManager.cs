ae0effddad726728ced72072928ef58f214a990a
jo-ji-hyun <newterr@naver.com>
2025-11-21 15:56:02 +0900

    [Feat] Addressable을 이용하여 이미지 변경

---
+++ Assets/03.Scripts/Manager/AddressableManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.AddressableAssets;
+
+public class AddressableManager : Singleton<AddressableManager>
+{
+    protected override bool IsDestroy => false;
+
+    public T GetAssets<T>(string key)
+    {
+        return Addressables.LoadAssetAsync<T>(key).WaitForCompletion();
+    }
+}
---
+++ Assets/03.Scripts/Ui/EquipUi.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class EquipUi : MonoBehaviour
+{
+    public Image Helmet_img;
+    public Image Weapon_img;
+    public Image Shield_img;
+    public Image Ring_img;
+
+    public void Start()
+    {
+        Helmet_img.sprite = AddressableManager.Instance.GetAssets<Sprite>("Assets/00.Externals/myimage/itemSheet0.png[itemSheet0_2]");
+        Weapon_img.sprite = AddressableManager.Instance.GetAssets<Sprite>("Assets/00.Externals/myimage/itemSheet0.png[itemSheet0_4]");
+        Shield_img.sprite = AddressableManager.Instance.GetAssets<Sprite>("Assets/00.Externals/myimage/itemSheet0.png[itemSheet0_0]");
+        Ring_img.sprite = AddressableManager.Instance.GetAssets<Sprite>("Assets/00.Externals/myimage/itemSheet0.png[itemSheet0_3]");
+    }
+}
