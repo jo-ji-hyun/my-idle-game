9cd19e0b09f8c965acb2c0ef69b26045edce5dfc
jo-ji-hyun <newterr@naver.com>
2025-11-22 16:30:31 +0900

    [Feat] 파이어베이스 초기화를 위한 작업
    + 자동으로 빌드에 도움되도록 enable

---
+++ Assets/03.Scripts/Utility/FirebaseController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using UnityEngine;
+using Firebase;
+using Firebase.Extensions;
+
+public class FirebaseController : MonoBehaviour
+{
+    public static bool IsReady { get; private set; }
+
+    public static event Action OnReady;
+
+    private static TaskCompletionSource<bool> _readyTcs = new TaskCompletionSource<bool>();
+
+    private void Awake()
+    {
+        Debug.Log("[Firebase] 의존성 확인 중…");
+        FirebaseApp.CheckAndFixDependenciesAsync().ContinueWithOnMainThread(t =>
+        {
+            IsReady = (t.Result == DependencyStatus.Available);
+            if (IsReady)
+            {
+                Debug.Log("[Firebase] 준비 완료");
+                _readyTcs.TrySetResult(true);
+                OnReady?.Invoke();
+            }
+            else
+            {
+                Debug.LogError($"[Firebase] 준비 실패: {t.Result}");
+                _readyTcs.TrySetResult(false);
+            }
+        });
+    }
+
+    public static Task<bool> WaitUntilReadyAsync() => _readyTcs.Task;
+}
