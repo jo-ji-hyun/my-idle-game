76a9ced9476830f3c42f806d2ded805255d10b89
jo-ji-hyun <newterr@naver.com>
2025-11-23 13:53:20 +0900

    [Feat] 계정을 받기 위한 준비

---
+++ Assets/03.Scripts/UI/AccountUi.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class AccountUi : MonoBehaviour
+{
+    public TMP_InputField IdInput;
+    public TMP_InputField PwInput;
+}
