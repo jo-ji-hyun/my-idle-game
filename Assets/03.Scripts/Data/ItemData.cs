de7949fb3ddda3a54b7f8fe2d093dd3198718b83
jo-ji-hyun <newterr@naver.com>
2025-12-22 17:06:22 +0900

    [Feat] 버전업 및 엑셀임포트를 위한 생성자 작성
    + 엑셀 번들 주소값 수정

--- Assets/03.Scripts/Data/ItemData.cs
+++ Assets/03.Scripts/Data/ItemData.cs
@@ -15,6 +15,8 @@ public class ItemData
     public Consts.ItemEnhanceCostType UpgradeType;   
     public string Icon;
 
+    public ItemData() { }
+
     public ItemData(ItemData other)
     {
         this.Id = other.Id;
