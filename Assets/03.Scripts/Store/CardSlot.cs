81f5fa444fe6f90d8268ec1c73e295beb4c2276f
jo-ji-hyun <newterr@naver.com>
2025-12-24 16:58:37 +0900

    [Feat] 10회 뽑기에도 적용

--- Assets/03.Scripts/Store/CardSlot.cs
+++ Assets/03.Scripts/Store/CardSlot.cs
@@ -5,13 +5,17 @@ using UnityEngine.UI;
 public class CardSlot : MonoBehaviour
 {
     public Image ItemSprite;
+    public Outline Outline;
     private int _enhanced;
     public TextMeshProUGUI CurrentEnhanced;
 
     public void SetInfo(ItemData data) 
     {
         ItemSprite.sprite = AddressableManager.Instance.GetAssets<Sprite>(data.Icon);
+        Outline.effectColor = Consts.ItemGradeColor(data.Grade);
+
         _enhanced = data.Enhanced;
         CurrentEnhanced.text = $"{_enhanced}";
+        CurrentEnhanced.color = Consts.ItemGradeColor(data.Grade);
     }
 }
