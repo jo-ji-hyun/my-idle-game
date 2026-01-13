2294b372d5d98100a3b59b10301aca1a28cbb655
jo-ji-hyun <newterr@naver.com>
2026-01-13 15:24:56 +0900

    [Feat] 도장 애니메이션 좀 더 길게 만듬

--- Assets/03.Scripts/PlayerReward/DailyCheckReward.cs
+++ Assets/03.Scripts/PlayerReward/DailyCheckReward.cs
@@ -59,7 +59,7 @@ public class DailyCheckReward : MonoBehaviour
         DaySlotList[todayindex].SetHighLight(true);
 
         // === 도장 애니메이션 ===
-        yield return new WaitForSeconds(2.0f);
+        yield return new WaitForSeconds(3.0f);
 
         StampSeal.transform.position = DaySlotList[todayindex].transform.position;
         StampSeal.transform.localScale = Vector3.one * 2.0f;
@@ -67,7 +67,7 @@ public class DailyCheckReward : MonoBehaviour
 
         SoundManager.Instance.SpecialEffectSound(Consts.SpecialItem.Stamp);
 
-        float stampTime = 0.1f;
+        float stampTime = 1.5f;
         float elapsed = 0f;
         while (elapsed < stampTime)
         {
