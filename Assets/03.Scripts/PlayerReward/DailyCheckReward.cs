5e8631431a9aa1db100a0522380f4d88dc3f4745
jo-ji-hyun <newterr@naver.com>
2026-01-16 16:07:01 +0900

    [Feat] 출석 애니메이션 속도 조절 및 연속 출석 일수 바로 갱신

--- Assets/03.Scripts/PlayerReward/DailyCheckReward.cs
+++ Assets/03.Scripts/PlayerReward/DailyCheckReward.cs
@@ -68,7 +68,7 @@ public class DailyCheckReward : MonoBehaviour
         DaySlotList[todayindex].SetHighLight(true);
 
         // === 도장 애니메이션 ===
-        yield return new WaitForSeconds(3.0f);
+        yield return new WaitForSeconds(1.0f);
 
         StampSeal.transform.position = DaySlotList[todayindex].transform.position;
         StampSeal.transform.localScale = Vector3.one * 2.0f;
@@ -76,7 +76,7 @@ public class DailyCheckReward : MonoBehaviour
 
         SoundManager.Instance.SpecialEffectSound(Consts.SpecialItem.Stamp);
 
-        float stampTime = 1.5f;
+        float stampTime = 1.0f;
         float elapsed = 0f;
         while (elapsed < stampTime)
         {
@@ -89,6 +89,8 @@ public class DailyCheckReward : MonoBehaviour
         StampSeal.transform.localScale = Vector3.one;
 
         GiveReward(today - 1);
+
+        UserAttendenceCalculationTxt.text = SaveManager.Instance.UserData.CumulativeAttendance.ToString();
 
         CloseBtn.gameObject.SetActive(true);
     }
